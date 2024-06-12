e61ca27cc5e339c7e1ee3a5aaefa6a8b5e8f7f46
Steven Snowball <wildplumsolutions@protonmail.com>
2024-06-12 22:05:27 +0100

    frontend starter load

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import { Container, Stack } from '@chakra-ui/react'
+import Navbar from './components/Navbar'
+import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
+
+function App() {
+
+  return (
+    <Stack h="100vh">
+      <Navbar />
+      <Container>
+        <TodoForm />
+        <TodoList />
+      </Container>
+    </Stack>
+  )
+}
+
+export default App
